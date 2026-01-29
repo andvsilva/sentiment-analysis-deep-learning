b5361c1f19e97d1e2cd48f30eb7111db6b168fb7
Andre V Silva <andsilvadrcc@gmail.com>
2026-01-28 21:32:30 -0300

    update repo. I will redo the for Sentiment Analysis with DL/LLMs

--- commit.sh
+++ commit.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # script to commit to repo.
-
 echo "${1}"
 
 echo "Add changes in files or directory..."
 git add .
 echo "committing..."
-
 git commit -m "${1}"
-git pull origin master
-git push -u origin master
-
+git pull
+git push --all
 echo "All done! :)"
+
