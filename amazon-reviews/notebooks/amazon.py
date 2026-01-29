64308488bcad00a0eaf45499c638b46fe4fa80eb
Andre V Silva <andsilvadrcc@gmail.com>
2026-01-28 22:28:08 -0300

    cleaning and organizing repo

---
+++ amazon-reviews/notebooks/amazon.py
@@ -0,0 +1,13 @@
+## libraries
+import time
+start_time = time.time()
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import date
+from datetime import datetime
+import os.path
+import sys
+import gc
